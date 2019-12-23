f3fcd8fffa9781e58b278d98ad9d9bd988c85f1a
Connor Brewster <connor.brewster@eagles.oc.edu>
2019-12-22 20:12:15 -0700

    Open a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("brewcode")
+        .build(&event_loop)?;
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::EventsCleared => {
+            window.request_redraw();
+        }
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => *control_flow = ControlFlow::Exit,
+
+        _ => *control_flow = ControlFlow::Poll,
+    });
 }
